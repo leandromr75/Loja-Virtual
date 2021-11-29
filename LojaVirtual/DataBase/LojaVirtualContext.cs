3452f8d2fc2b40bda48ddd77e7b0393644cd1bf1
unknown <leandromrxx75@gmail.com>
2021-11-29 13:22:40 -0300

    Configuração Banco de Dados - Parte 1

---
+++ LojaVirtual/DataBase/LojaVirtualContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LojaVirtual.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LojaVirtual.DataBase
+{
+    public class LojaVirtualContext : DbContext
+    {
+        public LojaVirtualContext(DbContextOptions<LojaVirtualContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
